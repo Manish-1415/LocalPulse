bbb315911aa5f65f5414d366723be2bcf62cf7f9
Manish Shinde <manishshinde1415@gmail.com>
2026-05-19 11:35:40 +0530

    User route is done adding with valid middlewares

---
+++ src/modules/user/user.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { createUser, getUser, newAccessToken, updatePassword, updateUser, userLogin, userLogOut } from "./user.controllers.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { validateRefToken } from "../../middlewares/verifyRefToken.middleware.js";
+import isGuest from "../../middlewares/isGuest.middleware.js";
+
+const router = Router();
+
+router.post("/register", isGuest, createUser);
+
+router.post("/login", isGuest, userLogin);
+
+router.post("/logout", authMiddleware, userLogOut);
+
+router.get("/getProfile/:id", authMiddleware ,getUser);
+
+router.patch("/update_profile/:id", authMiddleware, updateUser);
+
+router.patch("/update_password/:id", authMiddleware, updatePassword);
+
+router.post("/generate_token", validateRefToken, newAccessToken);
+
+export default router;
