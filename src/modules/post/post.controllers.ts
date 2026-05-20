9d9bbf1e4ee6b58d1cb2124b6d48dca71dc04331
Manish Shinde <manishshinde1415@gmail.com>
2026-05-20 18:46:18 +0530

    Controller added for post with create, update, fetch single,multiple & resolved posts also with delete, functionality

---
+++ src/modules/post/post.controllers.ts
@@ -0,0 +1,103 @@
+import type { Request, Response } from "express";
+import ApiResponse from "../../utility/ApiResponse.js";
+import type { CreatePostView, PostStatus } from "./post.types.js";
+import { createPostSchema, fetchPostsSchema, updatePostSchema, updatePostStatusSchema, type FetchPostsView, type UpdatePostInput } from "./post.validations.js";
+import postService from "./post.services.js";
+import type { JwtAccessPayload } from "../../utility/tokens.js";
+// import type { UpdatePostInput } from "./post.validations.js";
+import type { HydratedDocument } from "mongoose";
+import { type InferSchemaType } from "mongoose";
+import Post from "./post.model.js";
+
+export const createPost = async (req: Request, res: Response) => {
+  const postInfoObj: CreatePostView = createPostSchema.parse(req.body);
+  const user : string = req.user?._id!;
+
+  const post = await postService.createPostEntry(postInfoObj, user);
+
+  return res
+  .status(201)
+  .json(new ApiResponse(201, "Post Created Successfully", post));
+};
+
+
+export const getPost = async (req : Request<{id : string}, {}>, res : Response) => {
+  const postId : string = req.params.id;
+
+  const post = await postService.getPostEntry(postId);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Post Fetched Successfully", post));
+}
+
+
+export const updatePost = async (req : Request<{id : string}, {}, UpdatePostInput>, res : Response) => {
+  const postInfoObj : UpdatePostInput = updatePostSchema.parse(req.body);
+  const postId = req.params.id;
+  const userId = req.user?._id!;
+
+  const post = await postService.updatePostEntry(postId, postInfoObj, userId);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Post Updated Successfully", post));
+}
+
+
+export const fetchPosts = async (req : Request, res : Response) => {
+
+    const validatedData = fetchPostsSchema.parse({
+      lng: Number(req.params.lng),
+      lat: Number(req.params.lat),
+      radius: Number(req.params.radius),
+      category: req.params.category || undefined, // handles optional empty values
+      lastId: req.params.lastId || undefined,
+      sort: req.params.sort
+    });
+
+    const posts = await postService.fetchPostEntries(validatedData)
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Posts Fetched For User", posts));
+}
+
+
+export const fetchResolvedPost = async (req : Request<{id : string}>, res : Response) => {
+  const {id} = req.params;
+  const userPayload : JwtAccessPayload = req.user!;
+
+  const resolvedPosts = await postService.fetchResolvedPostEntries(id, userPayload);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Resolved Posts Fetched", resolvedPosts));
+}
+
+
+export const updatePostStatus = async (req : Request<{id : string}>, res : Response) => {
+
+  const status : PostStatus = updatePostStatusSchema.parse(req.body);
+
+  const post : HydratedDocument<InferSchemaType<typeof Post.schema>> = req.post;
+
+  const updatedPost = await postService.updatePostStatusEntry(post, status);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Post Status Updated", updatedPost));
+}
+
+
+
+export const deletePost = async (req : Request<{id : string}>, res : Response) => {
+  const postId = req.params.id;
+  const userId = req.user?._id!;
+
+  const post = await postService.deletePostEntry(postId, userId);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Post Deleted Successfully", {post : post.title, postId : post._id, message : "Post Deleted"}));
+}
