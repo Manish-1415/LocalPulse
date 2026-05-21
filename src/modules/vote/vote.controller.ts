05c468a449c534b24af3d022525e2b4b2ef548c0
Manish Shinde <manishshinde1415@gmail.com>
2026-05-21 13:10:06 +0530

    Controller Added for Vote module

---
+++ src/modules/vote/vote.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+import { toggleVoteSchema, type Vote } from "./vote.validation.js";
+import voteService from "./vote.services.js";
+import ApiResponse from "../../utility/ApiResponse.js";
+
+
+
+export const toggleVote = async (req : Request, res : Response) => {
+    const postId : string = req.params.post_id as string;
+    const userId : string = req.user?._id!;
+    const voteType : Vote = toggleVoteSchema.parse(req.body);
+
+    const vote = await voteService.toggleVoteEntry(postId, userId, voteType);
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, "Vote Toggled Successfully", vote));
+}
