2bf9fde8a97249abfda1a81206597282871320c2
Manish Shinde <manishshinde1415@gmail.com>
2026-05-22 15:53:55 +0530

    Routes added for admin module

---
+++ src/modules/admin/admin.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import isAdminMiddleware from "../../middlewares/isAdmin.middleware.js";
+import { deleteUser, getPendingReport, getSingleUser, getUsers, pendingReports, reportResolution, updatePostStatus } from "./admin.controllers.js";
+
+const router = Router();
+
+// users
+
+router.get("/get_users", authMiddleware, isAdminMiddleware, getUsers);
+
+router.get("/get_user/user_id", authMiddleware, isAdminMiddleware, getSingleUser);
+
+router.delete("delete_user/:user_id", authMiddleware, isAdminMiddleware, deleteUser);
+
+
+// posts 
+
+router.get("/get_posts", authMiddleware, isAdminMiddleware, getUsers);
+
+router.patch("update_post_status", authMiddleware, isAdminMiddleware, updatePostStatus);
+
+
+// reports
+
+router.get("/pending_reports", authMiddleware, isAdminMiddleware, pendingReports);
+
+router.get("/pending_report/:report_id", authMiddleware, isAdminMiddleware, getPendingReport);
+
+router.patch("/report_resolution/:report_id", authMiddleware, reportResolution);
+
+export default router;
