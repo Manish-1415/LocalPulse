f33b96abed990c8ed12d0b3da709475342a8f2d0
Manish Shinde <manishshinde1415@gmail.com>
2026-05-25 10:57:42 +0530

    Created & Implemented Socket.IO functionality by creating http Server into Socket Servers

---
+++ src/configs/initializingSocketIoServer.ts
@@ -0,0 +1,62 @@
+import { Server } from "socket.io";
+// import { createServer } from "node:http";
+import ApiError from "../utility/ApiError.js";
+import { verifyAccessToken } from "../utility/tokens.js";
+import {Server as HttpServer} from "node:http";
+
+
+let io : Server;
+// HttpServer is blueprint of what createServer creates / returns. 
+const initSocket = (server : HttpServer) => {
+    io = new Server(server, {
+        cors : {
+            origin : "*",
+            methods : ["GET", "POST"],
+            credentials : true
+        }
+    });
+
+
+    io.use((socket, next) => {
+        try {
+
+            const accessToken = socket.handshake.auth.accessToken;
+
+            if(accessToken) {
+                const payload = verifyAccessToken(accessToken);
+                socket.userId = payload?._id!;
+                console.log(`Authenticated User Recognized - ${socket.id}`);
+            }
+            else {
+                console.log(`Guest User Connected`);
+            }
+
+        } catch (error) {
+            console.log(error);
+            next(new ApiError(400, "Invalid Token"));
+        }
+    })
+
+
+    io.on("connection", (socket) => {
+        console.log(`Client Connected as Socket - ${socket.id}`);
+
+        if(socket.userId) {
+            socket.join(`user${socket.userId.toString()}`);
+        }
+
+        socket.on("disconnect", () => {
+            console.log(`Socket Disconnected ${socket.id}`);
+        })
+    })
+}
+
+
+export default initSocket;
+
+
+export const getIo = () => {
+    if(!io) throw Error("Socket Server is not being Initialized");
+
+    return io;
+}
