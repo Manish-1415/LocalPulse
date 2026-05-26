037864fa92fc5b421188a639e3b48d3cdb116ae6
Manish Shinde <manishshinde1415@gmail.com>
2026-05-26 09:46:45 +0530

    External types added for Express Request & Socket Object

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,13 @@
 import { JwtAccessPayload } from "../utility/tokens.ts";
+import { HydratedDocument } from "mongoose";
+import Post from "../modules/post/post.model.ts";
 
 declare global {
-    namespace Express {
-        interface Request {
-            user? : JwtAccessPayload
-        }
+  namespace Express {
+    interface Request {
+      user?: JwtAccessPayload;
+      refTokenPayload?: { _id: string; refreshToken: string };
+      post?: HydratedDocument<InstanceType<typeof Post.schema>>;
     }
+  }
 }
